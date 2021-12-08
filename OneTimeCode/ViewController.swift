767740c2b17650ceeb0a507348fdea2a24d04d11
TimotiusLeonardo <lionato14@gmail.com>
2021-12-08 12:39:55 +0700

    Initial Commit

---
+++ OneTimeCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OneTimeCode
+//
+//  Created by Timotius Leonardo Lianoto on 08/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
